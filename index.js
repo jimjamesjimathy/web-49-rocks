6a97166e333892870640822b4ccdd888781d05cc
James Kirkwood <85250373+jimjamesjimathy@users.noreply.github.com>
2022-01-13 14:08:50 -0500

    Created a react app

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 require('dotenv').config();
-
+const path = require('path');
 const express = require('express');
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'client/build')))
+
 const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-    res.json('Howdy')
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
 });
 
 app.listen(port, () => {
